5d7913c31a2c8bde08de690ea05cf8fea8c70953
Melchiades <melchiades.blanco@live.com>
2020-11-09 11:07:33 -0300

    wikitable

--- wikipedia/wikipedia/spiders/wikitable.py
+++ wikipedia/wikipedia/spiders/wikitable.py
@@ -9,4 +9,17 @@ class WikitableSpider(Spider):
     start_urls = ['https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population']
 
     def parse(self, response):
-        pass
+        table =  response.xpath('//table[contains(@class, "wikitable sortable")]')[0]
+        trs = table.xpath('.//tr')[1:]
+
+        for tr in trs:
+            rank  = tr.xpath('./td[1]/text()').extract_first()
+            city  = tr.xpath('./td[2]/text()').extract_first()
+            state = tr.xpath('.//*[@class="flagicon"]/following-sibling::a/text()|'
+                             './/*[@class="flagicon"]/following-sibling::text()').extract_first()
+
+        yield {
+            'rank': rank.strip(),
+            'city': city, 
+            'state': state.strip()
+        }
