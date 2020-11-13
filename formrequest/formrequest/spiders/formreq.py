9382fca742f1f238211124a2c13d4fa12a1416c0
Melchiades <melchiades.blanco@live.com>
2020-11-13 19:44:29 -0300

    formrequest final version

--- formrequest/formrequest/spiders/formreq.py
+++ formrequest/formrequest/spiders/formreq.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from scrapy import Spider
 from scrapy.http import FormRequest
-
+ 
 class FormreqSpider(Spider):
     name = 'formreq'
     allowed_domains = ['www.hkexnews.hk']
     start_urls = ['https://www.hkexnews.hk/sdw/search/searchsdw.aspx']
-
+ 
     def parse(self, response):
         data = {'__EVENTTARGET': 'btnSearch',
         '__VIEWSTATE': response.xpath('//*[@name="__VIEWSTATE"]/@value').extract_first(),
@@ -16,10 +16,12 @@ class FormreqSpider(Spider):
         'sortDirection': 'desc',
         'txtShareholdingDate': '2020/11/09',
         'txtStockCode': '00001'}
-
-        page = FormRequest('https://www.hkexnews.hk/sdw/search/searchsdw.aspx', formdata=data)
-
+ 
+        yield FormRequest('https://www.hkexnews.hk/sdw/search/searchsdw.aspx', formdata=data, callback=self.parse_form_page)
+ 
+    def parse_form_page(self, response):
         #At this point, the result should be visible in response?
+        #Or should I use fetch to parse the page??? How?
         #PARSING PHASE
     
         table = response.xpath('//table[@class="table table-scroll table-sort table-mobile-list "]')
@@ -29,8 +31,8 @@ class FormreqSpider(Spider):
             name = tr.xpath('.//td[@class="col-participant-name"]/div[@class="mobile-list-body"]/text()').extract_first()
             address = tr.xpath('.//td[@class="col-address"]/div[@class="mobile-list-body"]/text()').extract_first()
             shareholding = tr.xpath('.//td[@class="col-shareholding text-right"]/div[@class="mobile-list-body"]/text()').extract_first()
-            number_issues = tr.xpath('.//td[@class="col-shareholding-percent text-right"]/div[@class="mobile-list-body"]').extract_first()
-
+            number_issues = tr.xpath('.//td[@class="col-shareholding-percent text-right"]/div[@class="mobile-list-body"]/text()').extract_first()
+ 
             yield {
                 'participant_id': participant_id,
                 'name': name,
---
+++ x3crawler/x3crawler/spiders/x3iterator.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from time import sleep
+from scrapy import Spider
+from selenium import webdriver
+from scrapy.selector import Selector
+from scrapy.http import Request
+from selenium.common.exceptions import NoSuchElementException
+ 
+
+
+class X3iteratorSpider(Spider):
+    name = 'x3iterator'
+    #allowed_domains = ['x3.com']
+    start_urls = ['https://intech.sageerpx3.com.br/auth/login/page']
+
+    def start_requests(self):
+        self.driver = webdriver.Chrome('C:\\Users\\melch\\Documents\\GitHub\\py_web_crawler\\LIB\\driver\\chromedriver.exe')
+        #self.driver.get('http://books.toscrape.com')
+        self.driver.get('https://intech.sageerpx3.com.br/auth/login/page')
+
+        self.driver.find_element_by_id('login').send_keys('melchiades.blanco')
+        self.driver.find_element_by_id('password').send_keys('work6JOB6')
+        self.driver.find_element_by_xpath(r'/html/body/div/div/div[1]/form/div[4]/input[1]').click()
+
+        #Wait for it to load
+        sleep(5)
+        #self.driver.find_element_by_css_selector('a[data-s-sitemap-link="restWebServices"]').get_attribute('href')
+
+        self.driver.get(r'https://intech.sageerpx3.com.br/syracuse-main/html/main.html?url=%2Fsdata%2Fsyracuse%2Fcollaboration%2Fsyracuse%2FrestWebServices%3Frepresentation%3DrestWebService.%24query')
+
+        sel = Selector(text=self.driver.page_source)
+        trs = sel.xpath('//table/tr')
+        for tr in trs:
+            nome = tr.xpath('.//td[@class="s-grid-cell s-filter-criteria-col s-inplace"][1]/div[@class="s-inplace-value-read"]/a[@class="s-inplace-link"]/text()').extract_first()
+            url_base = tr.xpath('.//td[@class="s-grid-cell s-filter-criteria-col s-inplace"][2]/div[@class="s-inplace-value-read"]/text()').extract_first()
+            tipo_conteudo = tr.xpath('.//td[@class="s-grid-cell s-filter-criteria-col s-filter-criteria-col-with-btn s-inplace"][1]/div[@class="s-inplace-value-read"]/text()').extract_first()
+            autenticacao = tr.xpath('.//td[@class="s-grid-cell s-filter-criteria-col s-filter-criteria-col-with-btn s-inplace"][2]/div[@class="s-inplace-value-read"]/text()').extract_first()
+
+            yield {
+                'nome': nome,
+                'url_base': url_base,
+                'tipo_conteudo': tipo_conteudo,
+                'autenticacao' : autenticacao
+            }
+
+
+        # while True:
+        #     try:
+        #         next_page = self.driver.find_element_by_xpath('/html/body/div/div/div/div/section/div[2]/div/ul/li/a[text()="next"]')
+                
+        #         sleep(3)
+        #         self.logger.info('Sleep for 3 secs')
+        #         next_page.click()
+        #         sel = Selector(text=self.driver.page_source)
+        #         books = sel.xpath('//h3/a/@href').extract()
+        #         for book in books:
+        #             url = 'http://books.toscrape.com/' + book
+        #             yield Request(url, callback=self.parse_book)
+                    
+        #     except NoSuchElementException:
+        #         self.logger.info('No more pages to load')
+        #         self.driver.quit()
+        #         break
+
+    def parse_book(self, response):
+        pass
